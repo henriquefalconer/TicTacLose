6286fa30dfadf404f239dc6412ac0fbc2279ea55
Henrique Falconer <henrique.falconer@polijunior.com.br>
2020-11-13 17:03:37 -0300

    simplify BottomArea

--- src/components/BottomArea/index.tsx
+++ src/components/BottomArea/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import { useGame } from '../../hooks/useGame';
-
 import { Container } from './styles';
 
 const BottomArea: React.FC = () => {
     return (
-        <Container>
-        </Container>
+        <Container />
     );
 }
 
