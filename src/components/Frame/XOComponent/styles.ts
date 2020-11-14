dd5a5e9e467d77ef41b212eefb2663285a63456a
Henrique Falconer <henrique.falconer@polijunior.com.br>
2020-11-14 00:30:53 -0300

    fix XO padding

--- src/components/Frame/XOComponent/styles.ts
+++ src/components/Frame/XOComponent/styles.ts
@@ -2,12 +2,13 @@ import styled from 'styled-components/native';
 
 export const ButtonContainer = styled.TouchableOpacity`
     flex: 1;
-    padding: 30px;
+    /* padding: 30px; */
     height: 100%;
 `;
 
 export const SymbolImage = styled.Image`
     flex: 1;
+    margin: 20%;
     width: auto;
     height: auto;
 `;
--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -5,7 +5,7 @@ import * as TicTacToeBrain from '../utilities/TicTacToeBrain';
 import { Player, PositionId, SymbolData } from '../interfaces/GameData';
 import GameData, { EmptyGameData } from '../utilities/GameData';
 
-const DIMENSIONS = 4;
+const DIMENSIONS = 3;
 
 interface OrientationContextData {
     gameData: GameData;
